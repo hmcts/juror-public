d971c3c72da0b1cc9494e1c04f87338d41f7ff53
Ian Fisher - CGI <I.fisher@cgi.com>
2024-02-28 12:35:03 +0000

    correct secret

--- server/lib/appinsights.js
+++ server/lib/appinsights.js
@@ -7,7 +7,7 @@ module.exports.AppInsights = class AppInsights {
     const isProd = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'prod';
 
     if (isProd) {
-      appInsights.setup(secretsConfig.get('secrets.juror-digital-vault.app-insights-connection-string'))
+      appInsights.setup(secretsConfig.get('secrets.juror.app-insights-connection-string'))
         .setAutoCollectConsole(true, true)
         .start();
     }
