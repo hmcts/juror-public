2970d4b4d23cb35fc1f8f488e20a953fb29f0050
Ricardo <140706972+ricardofreitasrocha@users.noreply.github.com>
2024-03-04 09:26:10 +0000

    add a log message

--- server/lib/appinsights.js
+++ server/lib/appinsights.js
@@ -5,6 +5,8 @@ module.exports.AppInsights = class AppInsights {
 
   constructor() {
     if (secretsConfig.get('secrets.juror.app-insights-connection-string')) {
+      console.log('Starting Appinsights')
+
       appInsights.setup(secretsConfig.get('secrets.juror.app-insights-connection-string'))
         .setAutoCollectConsole(true, true)
         .start();
