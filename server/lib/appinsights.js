140d4d5db80312ff04ec6aa0df1a302ecf754cb0
Ricardo <140706972+ricardofreitasrocha@users.noreply.github.com>
2024-03-04 09:22:59 +0000

    update appinsights

--- server/lib/appinsights.js
+++ server/lib/appinsights.js
@@ -4,9 +4,7 @@ const secretsConfig = require('config');
 module.exports.AppInsights = class AppInsights {
 
   constructor() {
-    const isDev = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'dev';
-
-    if (!isDev) {
+    if (secretsConfig.get('secrets.juror.app-insights-connection-string')) {
       appInsights.setup(secretsConfig.get('secrets.juror.app-insights-connection-string'))
         .setAutoCollectConsole(true, true)
         .start();
