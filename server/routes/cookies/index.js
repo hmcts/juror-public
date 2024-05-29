b2b225ce2eef9bff377a3ec3e32a42f595b20464
Christopher Powell <chris.powell@cgi.com>
2024-05-29 15:30:48 +0100

    JDB-5556 - Cookie policy update for Dynatrace

--- server/routes/cookies/index.js
+++ server/routes/cookies/index.js
@@ -4,7 +4,6 @@
   var controller = require('./cookie.controller');
 
   module.exports = function(app) {
-    app.get('/help/cookies', 'cookie.get', controller.index());
     app.get('/cookie-settings', 'cookie-settings.get', controller.getCookieSettings(app));
 
     app.post('/cookie-banner', 'cookie-banner.post', controller.submitCookieBanner(app));
