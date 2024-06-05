0ee9c209bfd92abbabf474c8948fa82d70808cda
Christopher Powell (CGI) <151015663+ChrisPowellcgi@users.noreply.github.com>
2024-06-05 11:12:18 +0100

    JDB-5586 - Login summons date passed page (#201)

--- server/routes/steps/01-login/index.js
+++ server/routes/steps/01-login/index.js
@@ -1,14 +1,23 @@
-;(function(){
+;(function () {
   'use strict';
 
-  var controller = require('./login.controller')
-    , utils = require('../../../lib/utils');
+  const controller = require('./login.controller');
+  const utils = require('../../../lib/utils');
 
-  module.exports = function(app) {
+  module.exports = function (app) {
     app.get('/steps/login', 'steps.login.get', utils.checkPageAccess(app, 2), controller.index(app));
     app.get('/steps/login/tp', 'steps.login.tp.get', utils.checkPageAccess(app, 2), controller.index(app));
-    app.get('/steps/login/replied', 'steps.login.replied.get', utils.checkPageAccess(app, 2), controller.getReplied(app));
-    app.get('/steps/login/replied/tp', 'steps.login.replied.tp.get', utils.checkPageAccess(app, 2), controller.getReplied(app));
+
+    app.get('/steps/login/replied', 'steps.login.replied.get',
+      utils.checkPageAccess(app, 2), controller.getReplied(app));
+    app.get('/steps/login/replied/tp', 'steps.login.replied.tp.get',
+      utils.checkPageAccess(app, 2), controller.getReplied(app));
+
+    app.get('/steps/login/summons-date', 'steps.login.summons-date.get',
+      utils.checkPageAccess(app, 2), controller.getSummonsDate(app));
+    app.get('/steps/login/summons-date/tp', 'steps.login.summons-date.tp.get',
+      utils.checkPageAccess(app, 2), controller.getSummonsDate(app));
+
     app.post('/steps/login', 'steps.login.post', controller.create(app));
   };
 
--- server/routes/steps/01-login/login.controller.js
+++ server/routes/steps/01-login/login.controller.js
@@ -3,24 +3,24 @@
  * GET    /    ->    index
  */
 
-;(function() {
+;(function () {
   'use strict';
 
-  var validate = require('validate.js')
-    , _ = require('lodash')
-    , secretsConfig = require('config')
-    , filters = require('../../../components/filters')
-    , texts_en = require('../../../../client/js/i18n/en.json')
-    , texts_cy = require('../../../../client/js/i18n/cy.json')
-    , authComponent = require('../../../components/auth')
-    , msgMappingsEn = require('../../../components/errors/message-mapping_en')
-    , msgMappingsCy = require('../../../components/errors/message-mapping_cy')
-    , utils = require('../../../lib/utils');
+  const validate = require('validate.js');
+  const _ = require('lodash');
+  const secretsConfig = require('config');
+  const filters = require('../../../components/filters');
+  const textsEn = require('../../../../client/js/i18n/en.json');
+  const textsCy = require('../../../../client/js/i18n/cy.json');
+  const authComponent = require('../../../components/auth');
+  const msgMappingsEn = require('../../../components/errors/message-mapping_en');
+  const msgMappingsCy = require('../../../components/errors/message-mapping_cy');
+  const utils = require('../../../lib/utils');
 
-  module.exports.index = function(app) {
-    return function(req, res) {
-      var tmpErrors
-        , backLinkUrl;
+  module.exports.index = function (app) {
+    return function (req, res) {
+      let tmpErrors;
+      let backLinkUrl;
 
       // If already logged in, redirect to inbox
       if (typeof res.locals.authentication !== 'undefined') {
@@ -41,105 +41,111 @@
       // Reset user session data
       req.session.user = {
         thirdParty: req.session.user.thirdParty,
-      }
+      };
 
       // Merge and then delete errors, prevents retention after pressing back link
       tmpErrors = _.cloneDeep(req.session.errors);
       delete req.session.errors;
 
-      backLinkUrl = 'steps.responder.type.get'
+      backLinkUrl = 'steps.responder.type.get';
 
-      return res.render('steps/01-login.njk', {
+      return res.render('steps/01-login/login.njk', {
         user: req.session.user,
         errors: {
-          title: filters.translate('VALIDATION.ERROR_TITLE', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
+          title: filters.translate('VALIDATION.ERROR_TITLE', (req.session.ulang === 'cy' ? textsCy : textsEn)),
           message: '',
           count: typeof tmpErrors !== 'undefined' ? Object.keys(tmpErrors).length : 0,
           items: tmpErrors,
         },
-        backLinkUrl: backLinkUrl
+        backLinkUrl: backLinkUrl,
       });
     };
   };
 
-  module.exports.create = function(app) {
-    return function(req, res) {
-      var validatorResult
-        , tmpSession
+  module.exports.create = function (app) {
+    return function (req, res) {
+      let validatorResult;
+      let tmpSession;
 
-        , authSuccess = function(resp) {
-          tmpSession = req.session;
+      let authSuccess = function (resp) {
+        tmpSession = req.session;
 
-          app.logger.info('Login attempt for juror number "' + req.body.jurorNumber + '" succeeded.', {
-            jurorNumber: req.body.jurorNumber,
-            jurorLastName: req.body.jurorLastName,
-            jurorPostcode: req.body.jurorPostcode,
-            jwt: req.session.authToken,
-            response: resp,
+        app.logger.info('Login attempt for juror number "' + req.body.jurorNumber + '" succeeded.', {
+          jurorNumber: req.body.jurorNumber,
+          jurorLastName: req.body.jurorLastName,
+          jurorPostcode: req.body.jurorPostcode,
+          jwt: req.session.authToken,
+          response: resp,
+        });
+
+        // Regenerate session
+        req.session.regenerate(function (err) {
+          if (err) {
+            throw err;
+          }
+
+          // Regenerate the session
+          req.session.authToken = tmpSession.authToken;
+          req.session.authKey = tmpSession.authKey;
+          req.session.ulang = tmpSession.ulang;
+          req.session.user = _.merge(tmpSession.user, {
+            jurorNumber: req.body['jurorNumber'],
+            jurorLastName: req.body['jurorLastName'],
+            jurorPostcode: req.body['jurorPostcode'],
           });
+          // redirect to confirmation of replying on behalf of someone`
+          // if selected, otherwise move on to your details.
+          if (req.session.user['thirdParty'] === 'Yes') {
+            return res.redirect(app.namedRoutes.build('branches.third.party.details.name.get'));
+          }
+          return res.redirect(app.namedRoutes.build('steps.your.details.get'));
+        });
 
-          // Regenerate session
-          req.session.regenerate(function(err) {
-            if (err) {
-              throw err;
-            }
+      };
 
-            // Regenerate the session
-            req.session.authToken = tmpSession.authToken;
-            req.session.authKey = tmpSession.authKey;
-            req.session.ulang = tmpSession.ulang;
-            req.session.user = _.merge(tmpSession.user, {
-              jurorNumber: req.body['jurorNumber'],
-              jurorLastName: req.body['jurorLastName'],
-              jurorPostcode: req.body['jurorPostcode'],
-            });
-            // redirect to confirmation of replying on behalf of someone`
-            // if selected, otherwise move on to your details.
-            if (req.session.user['thirdParty'] === 'Yes') {
-              return res.redirect(app.namedRoutes.build('branches.third.party.details.name.get'));
-            }
-            return res.redirect(app.namedRoutes.build('steps.your.details.get'));
-          });
-   
+      let authFailure = function (err) {
+        // eslint-disable-next-line max-len
+        app.logger.crit('Login attempt for juror number "' + req.body.jurorNumber + '" responded with ' + err.statusCode, {
+          jurorNumber: req.body.jurorNumber,
+          jurorLastName: req.body.jurorLastName,
+          jurorPostcode: req.body.jurorPostcode,
+          jwt: req.session.authToken,
+          error: (typeof err.originalError !== 'undefined') ? err.originalError : err,
+        });
 
+        // Add error feedback
+        req.session.errors = {
+          authentication: [{
+            summary: (req.session.ulang === 'cy' ? msgMappingsCy : msgMappingsEn).logon[err.error],
+          }],
+        };
+
+        if (err.error === 'USER_NOT_FOUND') {
+          req.session.errors.jurorNumber = [{
+            details: '',
+          }];
+          req.session.errors.jurorLastName = [{
+            details: '',
+          }];
+          req.session.errors.jurorPostcode = [{
+            details: '',
+          }];
         }
 
-        , authFailure = function(err) {
+        // Summons date has passed for the juror summons
+        if (err.statusCode === 403 && err.error.startsWith('Not allowed. Court Date has already passed')) {
           // eslint-disable-next-line max-len
-          app.logger.crit('Login attempt for juror number "' + req.body.jurorNumber + '" responded with ' + err.statusCode, {
-            jurorNumber: req.body.jurorNumber,
-            jurorLastName: req.body.jurorLastName,
-            jurorPostcode: req.body.jurorPostcode,
-            jwt: req.session.authToken,
-            error: (typeof err.originalError !== 'undefined') ? err.originalError : err
-          });
+          return res.redirect(app.namedRoutes.build(utils.getRedirectUrl('steps.login.summons-date', req.session.user.thirdParty)));
+        }
 
-          // Add error feedback
-          req.session.errors = {
-            authentication: [{
-              summary: (req.session.ulang === 'cy' ? msgMappingsCy : msgMappingsEn).logon[err.error],
-            }],
-          };
-
-          if (err.error === 'USER_NOT_FOUND') {
-            req.session.errors.jurorNumber = [{
-              details: '',
-            }];
-            req.session.errors.jurorLastName = [{
-              details: '',
-            }];
-            req.session.errors.jurorPostcode = [{
-              details: '',
-            }];
-          }
-
-          // Response already submitted - redirect to information page
-          if (err.statusCode === 409) {
-            return res.redirect(app.namedRoutes.build(utils.getRedirectUrl('steps.login.replied', req.session.user.thirdParty)));
-          }
-          // Other error - return to login page
-          return res.redirect(app.namedRoutes.build(utils.getRedirectUrl('steps.login', req.session.user.thirdParty)));
-        };
+        // Response already submitted - redirect to information page
+        if (err.statusCode === 409) {
+          // eslint-disable-next-line max-len
+          return res.redirect(app.namedRoutes.build(utils.getRedirectUrl('steps.login.replied', req.session.user.thirdParty)));
+        }
+        // Other error - return to login page
+        return res.redirect(app.namedRoutes.build(utils.getRedirectUrl('steps.login', req.session.user.thirdParty)));
+      };
 
       // Reset error and saved field sessions
       delete req.session.errors;
@@ -159,17 +165,32 @@
     };
   };
 
-  module.exports.getReplied = function() {
-    return function(req, res) {
+  module.exports.getReplied = function () {
+    return function (req, res) {
 
       // Reset user session data
       req.session.user = {
         thirdParty: req.session.user.thirdParty,
-      }
+      };
       delete req.session.errors;
 
       return res.render('steps/01-login/replied.njk', {
-        user: req.session.user
+        user: req.session.user,
+      });
+    };
+  };
+
+  module.exports.getSummonsDate = function () {
+    return function (req, res) {
+
+      // Reset user session data
+      req.session.user = {
+        thirdParty: req.session.user.thirdParty,
+      };
+      delete req.session.errors;
+
+      return res.render('steps/01-login/summons-date.njk', {
+        user: req.session.user,
       });
     };
   };
