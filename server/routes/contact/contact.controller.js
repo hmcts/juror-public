d0c6941477943fc697da180c4d5e331380b82326
Christopher Powell (CGI) <151015663+ChrisPowellcgi@users.noreply.github.com>
2024-12-05 14:19:22 +0000

    Release5 (#272)
    * Update yarn-audit-known-issues
    
    * JS-120 - Contact page Webchat script updates (#273)

--- server/routes/contact/contact.controller.js
+++ server/routes/contact/contact.controller.js
@@ -1,9 +1,20 @@
-;(function(){
+;(function () {
   'use strict';
 
-  module.exports.index = function() {
-    return function(req, res) {
-      return res.render('contact.njk');
-    }
+  module.exports.index = function () {
+    return function (req, res) {
+
+      let webChatUUID = process.env.WEB_CHAT_UUID || 'script_30142320647759e09fcf50.58043359';
+      let webChatChannel = process.env.WEB_CHAT_CHANNEL || 'JCSB Webchat';
+      let webChatChannelUUID = process.env.WEB_CHAT_CHANNEL_UUID || '-gQ6XyBuR02jGgGRk_Mazw';
+      let webChatTenant = process.env.WEB_CHAT_TENANT || 'aG1jdHN4MTAx';
+
+      return res.render('contact.njk', {
+        webChatUUID: webChatUUID,
+        webChatChannel: webChatChannel,
+        webChatChannelUUID: webChatChannelUUID,
+        webChatTenant: webChatTenant,
+      });
+    };
   };
 })();
