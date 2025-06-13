a951781a7e84443f3ba501468ffaeaed3d0f4400
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2025-06-13 13:26:45 +0100

    task/JS-403: Update Last name to be 25 chars in Front End apps (#302)
    * Update last name validation to allow up to 25 chars
    
    * Update lastName max length to 25

--- server/config/validation/your-details-name.js
+++ server/config/validation/your-details-name.js
@@ -1,13 +1,13 @@
-;(function(){
+;(function () {
   'use strict';
 
-  var filters = require('../../components/filters')
-    , texts_en = require('../../../client/js/i18n/en.json')
-    , texts_cy = require('../../../client/js/i18n/cy.json');
+  let filters = require('../../components/filters');
+  let texts_en = require('../../../client/js/i18n/en.json');
+  let texts_cy = require('../../../client/js/i18n/cy.json');
 
   require('./custom-validation');
 
-  module.exports = function(req) {
+  module.exports = function (req) {
     return {
       title: {
         format: {
@@ -32,22 +32,22 @@
           message: {
             summary: filters.translate('VALIDATION.YOUR_DETAILS.FIRST_NAME_MISSING', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
             details: filters.translate('VALIDATION.YOUR_DETAILS.FIRST_NAME_MISSING', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
-          }
+          },
         },
         length: {
           maximum: 20,
           message: {
             summary: filters.translate('VALIDATION.YOUR_DETAILS.FIRST_NAME_CHECK', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
             details: filters.translate('VALIDATION.YOUR_DETAILS.FIRST_NAME_CHECK', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
-          }
+          },
         },
         format: {
           pattern: '^$|^[^|"]+$',
           message: {
             summary: filters.translate('VALIDATION.YOUR_DETAILS.FIRST_NAME_CHECK', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
             details: filters.translate('VALIDATION.YOUR_DETAILS.FIRST_NAME_CHECK', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
-          }
-        }
+          },
+        },
       },
 
       lastName: {
@@ -56,21 +56,21 @@
           message: {
             summary: filters.translate('VALIDATION.YOUR_DETAILS.LAST_NAME_MISSING', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
             details: filters.translate('VALIDATION.YOUR_DETAILS.LAST_NAME_MISSING', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
-          }
+          },
         },
         length: {
-          maximum: 20,
+          maximum: 25,
           message: {
             summary: filters.translate('VALIDATION.YOUR_DETAILS.LAST_NAME_CHECK', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
             details: filters.translate('VALIDATION.YOUR_DETAILS.LAST_NAME_CHECK', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
-          }
+          },
         },
         format: {
           pattern: '^$|^[^|"]+$',
           message: {
             summary: filters.translate('VALIDATION.YOUR_DETAILS.LAST_NAME_CHECK', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
             details: filters.translate('VALIDATION.YOUR_DETAILS.LAST_NAME_CHECK', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
-          }
+          },
         },
       },
     };
