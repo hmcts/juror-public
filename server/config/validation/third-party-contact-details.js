915503ca04116e7d8d571e80c0eb0506920a239f
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2025-12-16 11:19:46 +0000

    Release5.17 (#353)
    * Update version no.
    
    * JS-691 - Node package security updates (#355)
    
    * JS-657 - Update axios log messages (#356)
    
    * JS-657 - revert appinsights dependency version (#359)
    
    * JS-657 - revert appinsights dependency version
    
    * Update yarn-audit-known-issues
    
    * task/JS-628: DAC Accessibility Updates (#347)
    
    * Update version no.
    
    * task/JS-616: Update breaking dependencies (#350)
    
    * Task/JS-557: Update dependencies (#337)
    
    * Update in range dependencies
    
    * Update dependency govuk-frontend to v5.12.0
    
    * Update Node.Js to 20.19.5
    
    * Update dependency config to v4
    
    * Update dependency copy-webpack-plugin to v13
    
    * Update dependency express-rate-limit to v8
    
    * Update dependency helmet to v8
    
    * Update dependency method-override to v3
    
    * Update dependency nunjucks-async-loader to v2
    
    * Replace helmet.noCache with nocache package
    
    * Update express.js
    
    * Update webpack-cli to v6.0.1
    
    * Downgrade eslint
    
    * Downgrade express-rate-limit to 7.5.1
    
    * Update yarn-audit-known-issues
    
    * Update yarn-audit-known-issues
    
    * fix audit
    
    * yarn-audit-known-issues
    
    * Update url-join to v5
    
    * Update sass-loader to v16.0.6
    
    * Update redis to v5.9.0
    
    * Update body-parser to v2.20
    
    * Update express v5.1.0
    
    * Update in range dependencies
    
    * Update express-rate-limit to v8.1.0
    
    * Update applicationinsights to v3.12.0
    
    * Update yarn-audit-known-issues
    
    ---------
    
    Co-authored-by: Christopher Powell (CGI) <151015663+ChrisPowellcgi@users.noreply.github.com>
    Co-authored-by: Neil Perry <neil.perry@cgi.com>
    
    * JS-642: redis update (#348)
    
    * Downgrade applicationinsights to v2.9.8
    
    * Downgrade url-join to v4.0.1
    
    * Update connect-redis to v8
    
    * JS-629 - Updates for analytics additional tracking (#351)
    
    * Update yarn-audit-known-issues
    
    * DAC Accessibility Updates
    
    * task/JS-628: DAC Welsh translations
    
    * Updates to datepicker / service title link
    
    * Phone error message changed
    
    * Service name link update
    
    * Optional Field Added
    
    * Downgrade applicationinsights to v2.9.8
    
    * Update yarn-audit-known-issues
    
    * DAC Accessibility Updates
    
    * task/JS-628: DAC Welsh translations
    
    * Updates to datepicker / service title link
    
    * Phone error message changed
    
    * Service name link update
    
    * Optional Field Added
    
    * Update yarn-audit-known-issues
    
    ---------
    
    Co-authored-by: Callum Young <callum.young@cgi.com>
    Co-authored-by: Callum Young <148960132+callum-r-young@users.noreply.github.com>
    Co-authored-by: Christopher Powell (CGI) <151015663+ChrisPowellcgi@users.noreply.github.com>
    Co-authored-by: Neil Perry <neil.perry@cgi.com>
    Co-authored-by: Neil Perry <142518199+npv0@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Christopher Powell (CGI) <151015663+ChrisPowellcgi@users.noreply.github.com>
    Co-authored-by: Christian <christian.cubos@hmcts.net>
    Co-authored-by: Neil Perry <neil.perry@cgi.com>
    Co-authored-by: Neil Perry <142518199+npv0@users.noreply.github.com>

--- server/config/validation/third-party-contact-details.js
+++ server/config/validation/third-party-contact-details.js
@@ -71,7 +71,7 @@
         format: {
           pattern: phoneRegEx,
           message: {
-            summary: filters.translate('VALIDATION.ON_BEHALF.THIRD_PARTY_CONTACT.PHONE_NUMBER_OTHER_CHECK', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
+            summary: filters.translate('VALIDATION.ON_BEHALF.THIRD_PARTY_CONTACT.PHONE_NUMBER_OTHER_CHECK_INVALID', (req.session.ulang === 'cy' ? texts_cy : texts_en)),
             details: filters.translate('VALIDATION.ON_BEHALF.THIRD_PARTY_CONTACT.PHONE_NUMBER_OTHER_CHECK_INVALID', (req.session.ulang === 'cy' ? texts_cy : texts_en))
           },
         },
