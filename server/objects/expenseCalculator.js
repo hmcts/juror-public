927c3bd1fef381cebfd88e20b5982f4258d21e12
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-04-28 13:39:29 +0100

    Update expenseCalculator.js

--- server/objects/expenseCalculator.js
+++ server/objects/expenseCalculator.js
@@ -1,16 +1,21 @@
 ;(function(){
   'use strict';
 
+  const _ = require('lodash');
   const { axiosInstance } = require('./axios-instance');
+  const { replaceAllObjKeys, basicDataTransform2 } = require('../lib/utils');
 
   const expenseCalculator = {
     resource: 'bureau/expenseCalculator/estimate',
     post: function(app, jwtToken, expenseData) {
 
       let url = this.resource;
-      let options = {'method': 'post'};
+      let options = {
+        'method': 'post',
+        transform: basicDataTransform2,
+      };
 
-      options.data = expenseData;
+      options.data = replaceAllObjKeys(expenseData, _.snakeCase);
 
       return axiosInstance(url, app, jwtToken, options);
     },
