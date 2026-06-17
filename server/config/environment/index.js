ede96e84d44178fc289a02fda28c1ec1339d7562
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-06-17 09:24:39 +0100

    Update index.js

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -43,8 +43,8 @@
         // rate limiting - defaults to 1 mil requests per minute
         rateLimitEnabled: true,
         rateLimit: {
-          time: process.env.RATE_LIMIT_TIME ? parseInt(process.env.RATE_LIMIT_TIME) : (1 * 60 * 1000), // time window in milliseconds
-          max: process.env.RATE_LIMIT_MAX ? parseInt(process.env.RATE_LIMIT_MAX) : 1000000, // max number of requests per time (in ms above)
+          time: process.env.RATE_LIMIT_TIME ? Number(process.env.RATE_LIMIT_TIME) : (1 * 60 * 1000), // time window in milliseconds
+          max: process.env.RATE_LIMIT_MAX ? Number(process.env.RATE_LIMIT_MAX) : 1000000, // max number of requests per time (in ms above)
           message: 'Too many requests, please try again later.',
         },
       };
